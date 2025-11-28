d451adfefb55b07c1d49a3514fbfa2264dec64b4
Filip Buljan <filipbuljan2002@gmail.com>
2025-11-28 20:11:58 +0100

    Frontend initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontSize: "2rem" }}>
+      <h1>Hello world 👋</h1>
+      <p>React + TypeScript + Vite</p>
+    </div>
+  );
+}
+
+export default App;
